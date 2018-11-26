44ff617d38776947f8b50ea102f9ea4d261925d2
miaomiaobobo <1625554940@qq.com>
2018-11-26 18:06:48 +0800

    苗波 单词添加（60个）

--- app/src/main/java/com/example/x2y/englishapp/PersonPage/BookActivity.java
+++ app/src/main/java/com/example/x2y/englishapp/PersonPage/BookActivity.java
@@ -2,6 +2,7 @@ package com.example.x2y.englishapp.PersonPage;
 
 //词典里面的具体内容
 import android.content.Intent;
+import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,13 +19,22 @@ import android.widget.ListView;
 import com.bumptech.glide.Glide;
 import com.example.x2y.englishapp.R;
 
+import org.apache.commons.lang3.ArrayUtils;
+import com.show.api.ShowApiRequest;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 public class BookActivity extends AppCompatActivity {
     public static final String BOOK_NAME="book_name";
     public static final String BOOK_IMAGE_ID="book_image_id";
+    protected Handler mHandler = new Handler();
+
+    final String data[]=new String[20];
+    String dataAll[]=new String[0];
 
     //数据来源，这个地方应该改成数据库的来源，根据传入的词典名字（BOOK_NAME）查询数据库
 
-    private String[] data={"ooop","ddd","dddd","gded","dfds","sdfs"
+    private String[] datao={"ooop","ddd","dddd","gded","dfds","sdfs"
             ,"sf","dsfd","sdfsd","kjh","yuity","uytu"};
 
 
@@ -51,11 +61,56 @@ public class BookActivity extends AppCompatActivity {
 
 
 
-        ListView bookContentText=(ListView)findViewById(R.id.book_content_text);
+      final   ListView bookContentText=(ListView)findViewById(R.id.book_content_text);
+if(bookName.equals("四级词汇")){
+    new Thread() {
+        //在新线程中发送网络请求
+        public void run() {
+            //  j为请求的次数（1-226），每次请求就会得到20个
+            for(int j=1;j<=3;j++) {
+                String appid = "81452";//要替换成自己的
+                String secret = "7a70115f47e1405e9644b274c7af6d7f";//要替换成自己的
+                final String res;
+                res = new ShowApiRequest("http://route.showapi.com/8-10", appid, secret)
+                        .addTextPara("class_id", "45090")
+                        .addTextPara("course", Integer.toString(j))
+                        .post();
 
+                //System.out.println(res);
+//把返回内容通过handler对象更新到界面
+//                        Message
+                JSONObject jsStr = JSONObject.parseObject(res);
+                final JSONObject listJ = JSONObject.parseObject(jsStr.getString("showapi_res_body"));
+                final JSONArray listJ2 = JSONArray.parseArray(listJ.getString("list")); //.parseObject(listJ.getString("list"));
+               // System.out.println(listJ2);
+                for (int i = 0; i < listJ2.size(); i++) {
+                    JSONObject jsonObject = (JSONObject) listJ2.get(i);
+                   // System.out.println(i + jsonObject.getString("name"));
+                    data[i] = jsonObject.getString("name");
+                                    /*TextView txt=(TextView)findViewById(R.id.textview1);
+                                    txt.setText(i + jsonObject.getString("name"));*/
+                }
+                dataAll=(String[])ArrayUtils.addAll(dataAll,data);
+            }
+            System.out.println(dataAll[50]);
+            mHandler.post(new Thread() {
+                public void run() {
+
+                    ArrayAdapter<String> madapter=new ArrayAdapter<String>(BookActivity.this,android.R.layout.simple_list_item_1,dataAll);
+                    bookContentText.setAdapter(madapter);
+                    setListViewHeightBasedOnChildren(bookContentText);
+                }
+            });
+        }
+    }.start();
+
+
+}
+    else {
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(
-                BookActivity.this,android.R.layout.simple_list_item_1,data);
+                BookActivity.this,android.R.layout.simple_list_item_1,datao);
         bookContentText.setAdapter(adapter);
+}
         setListViewHeightBasedOnChildren(bookContentText);
 
     }
